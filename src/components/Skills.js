071c19f0eef167c0b2c7e4712a50dc9d22d24c0a
inshalashraf <mdinshalashraf@gmail.com>
2026-09-12 16:31:03 +0530

    Refresh portfolio skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap"
 import Carousel from "react-multi-carousel"
 import "react-multi-carousel/lib/styles.css"
 import meter1 from "../assets/img/meter1.svg"
@@ -6,69 +6,47 @@ import meter2 from "../assets/img/meter2.svg"
 import meter3 from "../assets/img/meter3.svg"
 import colorSharp from "../assets/img/color-sharp.png"
 
-
 export const Skills = () => {
-
     const responsive = {
-        superLargeDesktop: {
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+        superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
+        desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
+        tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
+        mobile: { breakpoint: { max: 464, min: 0 }, items: 1 }
+    }
 
-    return(
+    const skills = [
+        [meter1, "Python"],
+        [meter3, "Java"],
+        [meter2, "C / C++"],
+        [meter2, "Data Structures & Algorithms"],
+        [meter2, "Web Development"]
+    ]
+
+    return (
         <section className="skill" id="skills">
             <Container>
                 <Row>
                     <Col>
                         <div className="skill-bx">
-                            <h2>
-                                Skills
-                            </h2>
+                            <h2>Skills</h2>
                             <p>
-                                I have mostly coded in C++, but in recent times I learned JAVA too. <br />
-                                    Also, am familiar with Web Development stuffs & Python.
+                                My current focus is software development and data science, with hands-on
+                                practice through academic and personal projects.
                             </p>
 
-                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                                <div className="item">
-                                    <img src={meter1} alt= "Image" />
-                                    <h5>C & C++</h5>
-                                </div>
-
-                                <div className="item">
-                                    <img src={meter3} alt= "Image" />
-                                    <h5>Java</h5>
-                                </div>
-
-                                <div className="item">
-                                    <img src={meter2} alt= "Image" />
-                                    <h5>Web Development</h5>
-                                </div>
-
-                                <div className="item">
-                                    <img src={meter2} alt= "Image" />
-                                    <h5>Python</h5>
-                                </div>
+                            <Carousel responsive={responsive} infinite className="skill-slider">
+                                {skills.map(([image, name]) => (
+                                    <div className="item" key={name}>
+                                        <img src={image} alt="" aria-hidden="true" />
+                                        <h5>{name}</h5>
+                                    </div>
+                                ))}
                             </Carousel>
                         </div>
                     </Col>
                 </Row>
             </Container>
-            <img className="background-image-left" src={colorSharp} />
-        </section> 
-
+            <img className="background-image-left" src={colorSharp} alt="" aria-hidden="true" />
+        </section>
     )
 }
