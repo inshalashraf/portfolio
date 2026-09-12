b77adf1dbe9757203c2a2a7ec85cb3084434adf3
inshalashraf <mdinshalashraf@gmail.com>
2026-09-12 16:43:54 +0530

    Build professional portfolio page structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { NavBar } from './components/NavBar'
 import { Banner } from './components/Banner'
+import { About } from './components/About'
+import { Experience } from './components/Experience'
 import { Skills } from './components/Skills'
 import { Projects } from './components/Projects'
 import { Contact } from './components/Contact'
@@ -11,8 +13,10 @@ function App() {
         <div className="App">
             <NavBar />
             <Banner />
-            <Skills />
+            <About />
+            <Experience />
             <Projects />
+            <Skills />
             <Contact />
         </div>
     )
