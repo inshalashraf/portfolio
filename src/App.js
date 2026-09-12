82656151b67039b48223fa6b0b117f487870dd72
inshalashraf <mdinshalashraf@gmail.com>
2026-09-12 16:31:21 +0530

    Add contact section to portfolio app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import { Skills } from './components/Skills';
-
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { NavBar } from './components/NavBar'
+import { Banner } from './components/Banner'
+import { Skills } from './components/Skills'
+import { Contact } from './components/Contact'
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Skills />
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar />
+            <Banner />
+            <Skills />
+            <Contact />
+        </div>
+    )
 }
 
-export default App;
+export default App
